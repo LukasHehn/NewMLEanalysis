fd3549dcd07449f012b6667ed1a6da8da7cd623d
LukasHehn <lukas.hehn@ik.fzk.de>
2013-07-10 17:32:23 +0200

    Added plot with WIMP spectrum and trigger efficiency for WIMP signal tests.

--- TestEricsWimpDistri.py
+++ TestEricsWimpDistri.py
@@ -1,47 +1,87 @@
 from ROOT import *
 from Functions import *
 
-gROOT.LoadMacro("/kalinka/home/hehn/PhD/LowMassEric/WimpDistri.C")
+#gROOT.LoadMacro("/kalinka/home/hehn/PhD/LowMassEric/WimpDistriExtended.C")
+gROOT.LoadMacro("/kalinka/home/hehn/PhD/LowMassEric/WimpDistriAdapted.C")
 
 FWHM_heat = 0.82
 FWHM_rec = RecoilResolutionFromHeat(FWHM_heat,6.4,10)
 sigma_rec = FWHM_rec/2.35
 FWHM_ion = 0.72
 
-wimp_mass = '15'
+wimp_mass = 10
+flag2cut = 1
+ioncut = 0
+reccut = 0
 detector = 'ID3'
+voltage = 6.4
 
 TriggerEfficiency.SetParameter(0, 3.874)
 TriggerEfficiency.SetParameter(1, sigma_rec)
 
-TriggerEfficiency.SetNpx(1000)
-efficiency = TriggerEfficiency.GetHistogram()
+TriggerEfficiency.SetNpx(200)
+TriggerEfficiencyHist = TriggerEfficiency.GetHistogram()
 
-signal = WimpDistri(wimp_mass, detector, FWHM_rec, FWHM_ion, efficiency, 0, 0, 0, 6.4, 1)
+spectrum = ReadInWimpSpectrumEric(str(wimp_mass))
+
+gammacut = 0
+signal = WimpDistri(str(wimp_mass), detector, FWHM_rec, FWHM_ion, TriggerEfficiencyHist, ioncut, reccut, gammacut, voltage, flag2cut)
+
+gammacut = 1
+signalcut = WimpDistri(str(wimp_mass), detector, FWHM_rec, FWHM_ion, TriggerEfficiencyHist, ioncut, reccut, gammacut, voltage, flag2cut)
 
 c1 = TCanvas('c1','WIMP signal test',1000,500)
-c1.Divide(2)
+c1.Divide(2,1,0.01,0.01)
 c1.cd(1)
-signal.Draw('CONT0')
-signal.SetContour(30)
+spectrum.SetTitle('WIMP Spectrum %i GeV'%wimp_mass)
+spectrum.SetStats(0)
+spectrum.GetXaxis().SetTitleSize(0.05)
+spectrum.GetXaxis().SetLabelSize(0.05)
+spectrum.GetXaxis().SetTitleOffset(1.2)
+spectrum.GetYaxis().SetTitleSize(0.05)
+spectrum.GetYaxis().SetLabelSize(0.05)
+spectrum.GetYaxis().SetTitleOffset(1.0)
+spectrum.Draw()
+gPad.Update()
 
-ER_centroid.SetLineColor(kMagenta)
-ER_centroid.SetLineWidth(2)
-ER_centroid.SetParameter(0,6.4)
-ER_centroid.DrawCopy('SAME')
+TriggerEfficiencyHist.Scale(gPad.GetUymax())
+rightmax = 1.0
+TriggerEfficiencyHist.Draw('SAME')
 
-NR_centroid.SetLineColor(kMagenta)
-NR_centroid.SetLineWidth(2)
-NR_centroid.DrawCopy('SAME')
-NR_centroid.Draw('SAME')
-
-#signal.GetXaxis().SetRangeUser(3.2,12.8)
-signal.GetXaxis().SetTitle('E_{rec} (keV_{nr})')
-#signal.GetYaxis().SetRangeUser(0.8,5.8)
-signal.GetYaxis().SetTitle('E_{ion} (keV_{ee})')
+axis1 = TGaxis(gPad.GetUxmax(),gPad.GetUymin(), gPad.GetUxmax(), gPad.GetUymax(),0,1.0,510,"+L")
+axis1.SetLineColor(kRed)
+axis1.SetLabelColor(kRed)
+axis1.SetTitleColor(kRed)
+axis1.SetTitle('Trigger Efficiency')
+axis1.SetTitleSize(0.05)
+axis1.SetLabelSize(0.05)
+axis1.SetTitleOffset(0.7)
+axis1.Draw()
 
 c1.cd(2)
-efficiency.Draw()
-TriggerEfficiency.Draw('SAME')
+signal.Draw('CONT0Z')
+signal.SetContour(50)
 
-print "integrated efficiency corrected signal rate [10^-6pb/kg/day]",signal.Integral('WIDTH')
+ER_centroid.SetLineColor(kBlack)
+ER_centroid.SetLineWidth(2)
+ER_centroid.SetParameter(0,6.4)
+ER_centroid.Draw('SAME')
+
+NR_centroid.SetLineColor(kBlack)
+NR_centroid.SetLineWidth(2)
+NR_centroid.Draw('SAME')
+#NR_centroid.Draw('SAME')
+
+#signal.GetXaxis().SetRangeUser(3.2,12.8)
+#signal.GetYaxis().SetRangeUser(0.8,5.8)
+
+signal.SetTitle('WIMP Signal %i GeV'%wimp_mass)
+signal.SetStats(0)
+signal.GetXaxis().SetTitle('E_{recoil} [keV_{nr}]')
+signal.GetYaxis().SetTitle('E_{ion} [keV_{ee}]')
+
+
+print 'wimp mass: {0:} GeV'.format(wimp_mass)
+print 'integral 1D rate:  {0:.2f} [10^-6pb/kg/day]'.format(spectrum.Integral('WIDTH'))
+print 'integral 2D rate:  {0:.2e} [10^-6pb/kg/day]'.format(signal.Integral('WIDTH'))
+print 'integral 2D rate (with gamma cut):  {0:.2e} [10^-6pb/kg/day]'.format(signalcut.Integral('WIDTH'))
