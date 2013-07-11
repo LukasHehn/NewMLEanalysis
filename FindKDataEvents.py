2c5fbdcaaba6b389398673777e249e8283b151ab
LukasHehn <lukas.hehn@ik.fzk.de>
2013-07-11 15:34:12 +0200

    Moved test scripts into new "Test" folder. Completely rewrote script to find event sample in KData. It is now independent of the DetectorClass and much faster and more transparent to the user.

--- FindKDataEvents.py
+++ FindKDataEvents.py
@@ -1,51 +1,106 @@
 #!/usr/bin/env python
 from DetectorClass import *
 from ROOT import *
-from Parameters import *
+import Functions
 
-ID = Detector('ID3')
 
-KDataFile = 'Run12_ID3_bckg_with_subrecords.root'
+# define parameters used for skimming event set
+DetectorName = 'ID3'
+KDataFile = 'Data/Run12_ID3_bckg_with_subrecords.root'
+OutFileName = 'Data/ID3_Recalibration_Test.txt'
+EnergyIonMax = 14
+EnergyRecMax = 25
+IonFactor = 1.0
+HeatFactor = 1.0
+Voltage = 6.4
 
-def FindKDataEvents(ID, KDataFile):
-  eventcounter = 0
-  infile = KDataReader(KDataFile)
-  event = infile.GetEvent()
-  entries = infile.GetEntries()
-  stamp = event.GetStamp()
-  for entry in range(entries):
-    infile.GetEntry(entry)
-    bolos = event.GetNumBolos()
+CUTS = EricsLowEnergyCuts['ID3']
+TimeList,RecList,IonList = [],[],[]
+eventcounter = 0
 
-    #for bolonum in range(bolos):
-    if bolos == 1:
-      bolonum = 0
+infile = KDataReader(KDataFile)
 
-      #bolo records
-      bolo = event.GetBolo(bolonum)
+# get first event and total number of events
+event = infile.GetEvent()
+entries = infile.GetEntries()
+print entries,"total number of events in file",KDataFile
 
-      EnergyIon = bolo.GetEnergyIonFiducial()
+#stamp = event.GetStamp()
+for entry in range(entries):
+  #print "entry",entry
+  infile.GetEntry(entry)
+  bolos = event.GetNumBolos()
 
-      #samba records
-      samba = bolo.GetSambaRecord()
+  #for bolonum in range(bolos):
+  if bolos == 1:
+    bolonum = 0
 
-      UnixTime = samba.GetNtpDateSec()
-      RunName = samba.GetRunName()
+    #bolo records
+    bolo = event.GetBolo(bolonum)
 
-      if bolo.GetDetectorName()==ID.GetName()\
-         and bolo.GetEventFlag()==2\
-         and bolo.TestCutsBit(6)==True\
-         and bolo.GetVoltageFlag>=0\
-         and bolo.GetIonPulseTimeOffset()<600:
-        EnergyRec = ID.GetNuclearRecoilEnergy(bolo.GetEnergyHeat(1), bolo.GetVoltageFlag())
-        if ID.IsGoodEvent(UnixTime, EnergyRec, EnergyIon) == True:
-          ID.AddEvent(UnixTime, EnergyRec, EnergyIon)
-          print 'time: {0:8}, E_rec: {1:4.1f}, E_ion: {2:4.1f}'.format(UnixTime, EnergyRec, EnergyIon)
+    EnergyIon = IonFactor*bolo.GetEnergyIonFiducial()
+    EnergyHeat = HeatFactor*bolo.GetEnergyHeat(1)
+
+    #samba records
+    samba = bolo.GetSambaRecord()
+    
+    EventNumber = samba.GetSambaEventNumber()
+
+    UnixTime = samba.GetNtpDateSec()
+    RunName = samba.GetRunName()
+
+    if bolo.GetDetectorName()==DetectorName\
+       and CUTS['Heat']['Min']<bolo.GetBaselineNoiseHeat(1)<CUTS['Heat']['Max']\
+       and CUTS['Coll1']['Min']<bolo.GetBaselineNoiseCollectrode(1)<CUTS['Coll1']['Max']\
+       and CUTS['Coll2']['Min']<bolo.GetBaselineNoiseCollectrode(2)<CUTS['Coll2']['Max']\
+       and CUTS['Veto1']['Min']<bolo.GetBaselineNoiseVeto(1)<CUTS['Veto1']['Max']\
+       and CUTS['Veto2']['Min']<bolo.GetBaselineNoiseVeto(2)<CUTS['Veto2']['Max']\
+       and CUTS['Guard1']['Min']<bolo.GetBaselineNoiseGuard(1)<CUTS['Guard1']['Max']\
+       and CUTS['Guard2']['Min']<bolo.GetBaselineNoiseGuard(2)<CUTS['Guard2']['Max']\
+       and bolo.GetEventFlag()==2\
+       and bolo.GetVoltageFlag>=0\
+       and bolo.TestCutsBit(6)==True\
+       and bolo.GetIonPulseTimeOffset()<600:
+        EnergyRec = Functions.GetEnergyRecoilFromEstimator(EnergyHeat, Voltage)
+        if 0 < EnergyIon < EnergyIonMax and 0 < EnergyRec < EnergyRecMax:
+          TimeList.append(UnixTime)
+          IonList.append(EnergyIon)
+          RecList.append(EnergyRec)
+          print 'Event: {0:6}, time: {1:8}, E_heat: {2:4.1f}, E_rec: {3:4.1f}, E_ion: {4:4.1f}'.format(EventNumber, UnixTime, EnergyHeat, EnergyRec, EnergyIon)
           eventcounter += 1
 
-  infile.Close()
+infile.Close()
 
-  return "KData: %i events read" % eventcounter
+print "KData: %i events passed all cuts" % eventcounter
 
-  # save events to txt-file
-  #ID.WriteEventsToFile('ID3-eventlist_new.txt')
+# write events in txt-file
+if OutFileName:
+  OutFile = open(OutFileName, 'w')
+  for i in range(len(TimeList)):
+    UnixTime = TimeList[i]
+    YearTime = unixtime_to_year(UnixTime)
+    EnergyRec = RecList[i]
+    EnergyIon = IonList[i]
+    OutFile.write("%s %s %s \n" % (YearTime, EnergyRec, EnergyIon))
+  OutFile.close()
+  print 'Outfile %s written to disc' % OutFileName
+  
+  
+# fill events in E_rec/E_ion-TGraph and plot them on canvas
+EventGraph = TGraph()
+for i in range(eventcounter):
+  EnergyRec = RecList[i]
+  EnergyIon = IonList[i]
+  EventGraph.SetPoint(i,EnergyRec,EnergyIon)
+
+EventGraph.GetXaxis().SetTitle('E_{rec} [keVnr]')
+EventGraph.GetYaxis().SetTitle('E_{ion} [keVee]')
+
+c1 = TCanvas('c1','Event selection for ID3',800,600)
+EventGraph.SetMarkerStyle(kPlus)
+EventGraph.Draw('AP')
+
+# add lines for Electron Recoil and Nuclear Recoil centroids
+Functions.ER_centroid.SetParameter(0,6.4)
+Functions.ER_centroid.Draw('SAME')
+Functions.NR_centroid.Draw('SAME')
