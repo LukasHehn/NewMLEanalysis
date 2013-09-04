6c85081d2b844e4a0b738c04654d94d860d7eb88
LukasHehn <lukas.hehn@ik.fzk.de>
2013-09-04 10:40:52 +0200

    Renaming modules to lower case

--- mle.py
+++ mle.py
@@ -17,8 +17,8 @@ from ROOT import RooFit as rf
 DATA_FILE = '/kalinka/home/hehn/PhD/LowMassEric/ID3_eventlist.txt'
 E_ION_MAX = 10.
 E_REC_MAX = 20.
-WIMP_MASS = 12
-NUM_MC_SETS = int(1e4)  # number of MC toy event sets: 0 means no MC study
+WIMP_MASS = 15
+NUM_MC_SETS = int(1e1)  # number of MC toy event sets: 0 means no MC study
 SAVE_PLOTS = True
 
 
@@ -27,18 +27,18 @@ VOLTAGE = 6.4
 E_THRESH = 3.874
 FWHM_HEAT = 0.82  # value for ID3 adopted from Eric
 FWHM_ION = 0.72  # value for ID3 adopted from Eric
-FWHM_REC = Functions.RecoilResolutionFromHeat(FWHM_HEAT, VOLTAGE, 10.)
+FWHM_REC = functions.fwhm_rec_from_heat(FWHM_HEAT, VOLTAGE, 10.)
 
 
 # Definition of maximum likelihood observables in RooFit
 ION = ROOT.RooRealVar('ion', 'E_{ion}', 0., E_ION_MAX, 'keV_{ee}')
-REC = ROOT.RooRealVar('SIGMA_REC', 'E_{rec}', 0., E_REC_MAX, 'keV_{nr}')
+REC = ROOT.RooRealVar('rec', 'E_{rec}', 0., E_REC_MAX, 'keV_{nr}')
 SIGMA_REC = ROOT.RooRealVar('sigma_rec', 'recoil energy resolution', FWHM_REC/2.35)
 SIGMA_ION = ROOT.RooRealVar('sigma_ion', 'ionization energy resolution', FWHM_ION/2.35)
 
 
 # Calculation of specific detector efficiency and pdf
-total_efficiency = Functions.efficiency_ID3(E_THRESH, FWHM_REC/2.35)
+total_efficiency = functions.efficiency_ID3(E_THRESH, FWHM_REC/2.35)
 total_efficiency_datahist = ROOT.RooDataHist('total_efficiency_datahist', 'total_efficiency_datahist', 
                                              ROOT.RooArgList(REC, ION), total_efficiency)
 total_efficiency_pdf = ROOT.RooHistPdf('total_efficiency_pdf', 'total_efficiency_pdf', 
@@ -48,7 +48,7 @@ total_efficiency_pdf = ROOT.RooHistPdf('total_efficiency_pdf', 'total_efficiency
 # Read in of data set and output as scatter/graph
 realdata = ROOT.RooDataSet.read(DATA_FILE, ROOT.RooArgList(REC, ION))
 realdata_scatter = realdata.createHistogram(REC, ION, int(E_REC_MAX*10), int(E_ION_MAX*10))
-realdata_graph = Functions.tgraph_from_dataset(realdata)
+#realdata_graph = functions.tgraph_from_dataset(realdata)  # tgraph not really more accurate than binned scatter
 events = int(realdata.numEntries())
 
 
@@ -57,12 +57,12 @@ ion_scaling = ROOT.RooRealVar('ion_scaling', 'scaling factor ionization energy',
 rec_scaling = ROOT.RooRealVar('rec_scaling', 'scaling factor recoil energy', 1.0, 0.95, 1.05)
 ion_scaling.setConstant(ROOT.kTRUE)
 rec_scaling.setConstant(ROOT.kTRUE)
-Functions.ER_centroid.SetParameter(0, 6.4)  # ER_centroid for calculation of peak position in Erec
+functions.ER_CENTROID.SetParameter(0, 6.4)  # ER_centroid for calculation of peak position in Erec
 
 V49_ion_energy = ROOT.RooRealVar('V49_ion_energy', 'V49 peak ion energy', 4.97)
 V49_ion_pos = ROOT.RooFormulaVar('V49_ion_pos', '@0*@1', ROOT.RooArgList(V49_ion_energy, ion_scaling))
 V49_ion_pdf = ROOT.RooGaussian('V49_ion_pdf', 'V49 peak gauss pdf in ion', ION, V49_ion_pos, SIGMA_ION)
-V49_rec_energy = ROOT.RooRealVar('V49_rec_energy', 'recoil energy V49 peak', Functions.ER_centroid.GetX(V49_ion_energy.getVal()))
+V49_rec_energy = ROOT.RooRealVar('V49_rec_energy', 'recoil energy V49 peak', functions.ER_CENTROID.GetX(V49_ion_energy.getVal()))
 V49_rec_pos = ROOT.RooFormulaVar('V49_rec_pos', '@0*@1', ROOT.RooArgList(V49_rec_energy, rec_scaling))
 V49_rec_pdf = ROOT.RooGaussian('V49_rec_pdf', 'V49 peak pdf in recoil energy', REC, V49_rec_pos, SIGMA_REC)
 V49_pdf = ROOT.RooProdPdf('V49_pdf', 'V49 peak pdf', V49_ion_pdf, V49_rec_pdf)
@@ -72,7 +72,7 @@ N_V49 = ROOT.RooRealVar('N_V49', 'evts of V49 peak (4.97keV)', 16., 0., events)
 Cr51_ion_energy = ROOT.RooRealVar('Cr51_ion_energy', 'Cr51 peak ion energy', 5.46)
 Cr51_ion_pos = ROOT.RooFormulaVar('Cr51_ion_pos', '@0*@1', ROOT.RooArgList(Cr51_ion_energy, ion_scaling))
 Cr51_ion = ROOT.RooGaussian('Cr51_ion_pdf', 'Cr51 peak gauss pdf with shifted mean', ION, Cr51_ion_pos, SIGMA_ION)
-Cr51_rec_energy = ROOT.RooRealVar('Cr51_rec_energy', 'v_rec_energy', Functions.ER_centroid.GetX(Cr51_ion_energy.getVal()))
+Cr51_rec_energy = ROOT.RooRealVar('Cr51_rec_energy', 'v_rec_energy', functions.ER_CENTROID.GetX(Cr51_ion_energy.getVal()))
 Cr51_rec_pos = ROOT.RooFormulaVar('Cr51_rec_pos', '@0*@1', ROOT.RooArgList(Cr51_rec_energy, rec_scaling))
 Cr51_rec = ROOT.RooGaussian('Cr51_rec_pdf', 'Cr51_rec_pdf with shifted mean', REC, Cr51_rec_pos, SIGMA_REC)
 Cr51_pdf = ROOT.RooProdPdf('Cr51_pdf', 'Cr51 peak pdf', Cr51_ion, Cr51_rec)
@@ -81,7 +81,7 @@ N_Cr51 = ROOT.RooRealVar('N_Cr51', 'evts of 51Cr peak (5.46keV)', 11., 0., event
 Mn54_ion_energy = ROOT.RooRealVar('Mn54_ion_energy', 'Mn54_ion_energy', 5.99)
 Mn54_ion_pos = ROOT.RooFormulaVar('Mn54_ion_pos', '@0*@1', ROOT.RooArgList(Mn54_ion_energy, ion_scaling))
 Mn54_ion = ROOT.RooGaussian('Mn54_ion_pdf', 'Mn54_ion_pdf with shifted mean', ION, Mn54_ion_pos, SIGMA_ION)
-Mn54_rec_energy = ROOT.RooRealVar('Mn54_rec_energy', 'Mn54_rec_energy', Functions.ER_centroid.GetX(Mn54_ion_energy.getVal()))
+Mn54_rec_energy = ROOT.RooRealVar('Mn54_rec_energy', 'Mn54_rec_energy', functions.ER_CENTROID.GetX(Mn54_ion_energy.getVal()))
 Mn54_rec_pos = ROOT.RooFormulaVar('Mn54_rec_pos', '@0*@1', ROOT.RooArgList(Mn54_rec_energy, rec_scaling))
 Mn54_rec = ROOT.RooGaussian('Mn54_rec_pdf', 'Mn54_rec_pdf with shifted mean', REC, Mn54_rec_pos, SIGMA_REC)
 Mn54_pdf = ROOT.RooProdPdf('Mn54_pdf', 'Mn54 peak pdf', Mn54_ion, Mn54_rec)
@@ -90,7 +90,7 @@ N_Mn54 = ROOT.RooRealVar('N_Mn54', 'evts of 54Mn peak (5.99keV)', 4., 0., events
 Fe55_ion_energy = ROOT.RooRealVar('Fe55_ion_energy', 'Fe55_ion_energy', 6.54)
 Fe55_ion_pos = ROOT.RooFormulaVar('Fe55_ion_pos', '@0*@1', ROOT.RooArgList(Fe55_ion_energy, ion_scaling))
 Fe55_ion = ROOT.RooGaussian('Fe55_ion_pdf', 'Fe55_ion_pdf with shifted mean', ION, Fe55_ion_pos, SIGMA_ION)
-Fe55_rec_energy = ROOT.RooRealVar('Fe55_rec_energy', 'Fe55_rec_energy', Functions.ER_centroid.GetX(Fe55_ion_energy.getVal()))
+Fe55_rec_energy = ROOT.RooRealVar('Fe55_rec_energy', 'Fe55_rec_energy', functions.ER_CENTROID.GetX(Fe55_ion_energy.getVal()))
 Fe55_rec_pos = ROOT.RooFormulaVar('Fe55_rec_pos', '@0*@1', ROOT.RooArgList(Fe55_rec_energy, rec_scaling))
 Fe55_rec = ROOT.RooGaussian('Fe55_rec_pdf', 'Fe55_rec_pdf with shifted mean', REC, Fe55_rec_pos, SIGMA_REC)
 Fe55_pdf = ROOT.RooProdPdf('Fe55_pdf', 'Fe55 peak pdf', Fe55_ion, Fe55_rec)
@@ -99,7 +99,7 @@ N_Fe55 = ROOT.RooRealVar('N_Fe55', 'evts of 55Fe peak (6.54keV)', 31., 0., event
 Co57_ion_energy = ROOT.RooRealVar('Co57_ion_energy', 'Co57_ion_energy', 7.11)
 Co57_ion_pos = ROOT.RooFormulaVar('Co57_ion_pos', '@0*@1', ROOT.RooArgList(Co57_ion_energy, ion_scaling))
 Co57_ion = ROOT.RooGaussian('Co57_ion_pdf', 'Co57_ion_pdf with shifted mean', ION, Co57_ion_pos, SIGMA_ION)
-Co57_rec_energy = ROOT.RooRealVar('Co57_rec_energy', 'Co57_rec_energy', Functions.ER_centroid.GetX(Co57_ion_energy.getVal()))
+Co57_rec_energy = ROOT.RooRealVar('Co57_rec_energy', 'Co57_rec_energy', functions.ER_CENTROID.GetX(Co57_ion_energy.getVal()))
 Co57_rec_pos = ROOT.RooFormulaVar('Co57_rec_pos', '@0*@1', ROOT.RooArgList(Co57_rec_energy, rec_scaling))
 Co57_rec = ROOT.RooGaussian('Co57_rec_pdf', 'Co57_rec_pdf with shifted mean', REC, Co57_rec_pos, SIGMA_REC)
 Co57_pdf = ROOT.RooProdPdf('Co57_pdf', 'Co57 peak pdf', Co57_ion, Co57_rec)
@@ -108,7 +108,7 @@ N_Co57 = ROOT.RooRealVar('N_Co57', 'evts of 57Co peak (7.11keV)', 2., 0., events
 Zn65_ion_energy = ROOT.RooRealVar('Zn65_ion_energy', 'Zn65_ion_energy', 8.98)
 Zn65_ion_pos = ROOT.RooFormulaVar('Zn65_ion_pos', '@0*@1', ROOT.RooArgList(Zn65_ion_energy, ion_scaling))
 Zn65_ion = ROOT.RooGaussian('Zn65_ion_pdf', 'Zn65_ion_pdf with shifted mean', ION, Zn65_ion_pos, SIGMA_ION)
-Zn65_rec_energy = ROOT.RooRealVar('Zn65_rec_energy', 'Zn65_rec_energy', Functions.ER_centroid.GetX(Zn65_ion_energy.getVal()))
+Zn65_rec_energy = ROOT.RooRealVar('Zn65_rec_energy', 'Zn65_rec_energy', functions.ER_CENTROID.GetX(Zn65_ion_energy.getVal()))
 Zn65_rec_pos = ROOT.RooFormulaVar('Zn65_rec_pos', '@0*@1', ROOT.RooArgList(Zn65_rec_energy, rec_scaling))
 Zn65_rec = ROOT.RooGaussian('Zn65_rec_pdf', 'Zn65_rec_pdf with shifted mean', REC, Zn65_rec_pos, SIGMA_REC)
 Zn65_pdf = ROOT.RooProdPdf('Zn65_pdf', 'Zn65 peak pdf', Zn65_ion, Zn65_rec)
@@ -117,7 +117,7 @@ N_Zn65 = ROOT.RooRealVar('N_Zn65', 'evts of 65Zn peak (8.98keV)', 110., 0., even
 Ga68_ion_energy = ROOT.RooRealVar('Ga68_ion_energy', 'Ga68_ion_energy', 9.66)
 Ga68_ion_pos = ROOT.RooFormulaVar('Ga68_ion_pos', '@0*@1', ROOT.RooArgList(Ga68_ion_energy, ion_scaling))
 Ga68_ion = ROOT.RooGaussian('Ga68_ion_pdf', 'Ga68_ion_pdf with shifted mean', ION, Ga68_ion_pos, SIGMA_ION)
-Ga68_rec_energy = ROOT.RooRealVar('Ga68_rec_energy', 'Ga68_rec_energy', Functions.ER_centroid.GetX(Ga68_ion_energy.getVal()))
+Ga68_rec_energy = ROOT.RooRealVar('Ga68_rec_energy', 'Ga68_rec_energy', functions.ER_CENTROID.GetX(Ga68_ion_energy.getVal()))
 Ga68_rec_pos = ROOT.RooFormulaVar('Ga68_rec_pos', '@0*@1', ROOT.RooArgList(Ga68_rec_energy, rec_scaling))
 Ga68_rec = ROOT.RooGaussian('Ga68_rec_pdf', 'Ga68_rec_pdf with shifted mean', REC, Ga68_rec_pos, SIGMA_REC)
 Ga68_pdf = ROOT.RooProdPdf('Ga68_pdf', 'Ga68 peak pdf', Ga68_ion, Ga68_rec)
@@ -126,7 +126,7 @@ N_Ga68 = ROOT.RooRealVar('N_Ga68', 'evts of 68Ga peak (9.66keV)', 32., 0., event
 Ge68_ion_energy = ROOT.RooRealVar('Ge68_ion_energy', 'Ge68_ion_energy', 10.37)
 Ge68_ion_pos = ROOT.RooFormulaVar('Ge68_ion_pos', '@0*@1', ROOT.RooArgList(Ge68_ion_energy, ion_scaling))
 Ge68_ion = ROOT.RooGaussian('Ge68_ion_pdf', 'Ge68_ion_pdf with shifted mean', ION, Ge68_ion_pos, SIGMA_ION)
-Ge68_rec_energy = ROOT.RooRealVar('Ge68_rec_energy', 'Ge68_rec_energy', Functions.ER_centroid.GetX(Ge68_ion_energy.getVal()))
+Ge68_rec_energy = ROOT.RooRealVar('Ge68_rec_energy', 'Ge68_rec_energy', functions.ER_CENTROID.GetX(Ge68_ion_energy.getVal()))
 Ge68_rec_pos = ROOT.RooFormulaVar('Ge68_rec_pos', '@0*@1', ROOT.RooArgList(Ge68_rec_energy, rec_scaling))
 Ge68_rec = ROOT.RooGaussian('Ge68_rec_pdf', 'Ge68 peak pdf in rec', REC, Ge68_rec_pos, SIGMA_REC)
 Ge68_pdf = ROOT.RooProdPdf('Ge68_pdf', 'Ge68 peak pdf', Ge68_ion, Ge68_rec)
@@ -134,7 +134,7 @@ N_Ge68 = ROOT.RooRealVar('N_Ge68', 'evts of 68Ge peak (10.37keV)', 0., 0., event
 
 
 # Definition of WIMP signal and pdf
-signal_hist = Functions.construct_wimp_signal(WIMP_MASS, SIGMA_ION.getVal(), SIGMA_REC.getVal())
+signal_hist = functions.wimp_signal(WIMP_MASS, SIGMA_ION.getVal(), SIGMA_REC.getVal())
 signal_hist.Multiply(total_efficiency)
 signal_datahist = ROOT.RooDataHist('signal_datahist', 'signal_datahist', 
                                    ROOT.RooArgList(REC, ION), signal_hist)
@@ -144,7 +144,7 @@ N_signal = ROOT.RooRealVar('N_signal', 'WIMP signal events', 0., 0., 10.)
 
 
 # Definition of flat gamma background pdf
-flat_gamma_bckgd_hist = Functions.flat_gamma_bckgd(SIGMA_ION.getVal(), SIGMA_REC.getVal())
+flat_gamma_bckgd_hist = functions.flat_gamma_bckgd(SIGMA_ION.getVal(), SIGMA_REC.getVal())
 flat_gamma_bckgd_hist.Multiply(total_efficiency)
 flat_gamma_bckgd_datahist = ROOT.RooDataHist('flat_gamma_bckgd_datahist', 'flat_gamma_bckgd_datahist', 
                                              ROOT.RooArgList(REC, ION), flat_gamma_bckgd_hist)
@@ -272,22 +272,22 @@ c1.cd(1)
 c1.cd(1).SetLogz()
 bckgd_and_sig_hist.Draw('COL')
 bckgd_and_sig_hist.SetStats(0)
-realdata_graph.SetMarkerColor(ROOT.kBlack)
-realdata_graph.SetMarkerStyle(ROOT.kFullDotLarge)
-realdata_graph.Draw('SAMESP')
-Functions.ER_centroid.SetLineColor(ROOT.kBlack)
-Functions.ER_centroid.SetLineWidth(1)
-Functions.ER_centroid.DrawCopy('SAME')
-Functions.NR_centroid.SetLineColor(ROOT.kBlack)
-Functions.NR_centroid.SetLineWidth(1)
-Functions.NR_centroid.DrawCopy('SAME')
+realdata_scatter.SetMarkerColor(ROOT.kBlack)
+realdata_scatter.SetMarkerStyle(ROOT.kFullDotLarge)
+realdata_scatter.Draw('SAMESP')
+functions.ER_CENTROID.SetLineColor(ROOT.kBlack)
+functions.ER_CENTROID.SetLineWidth(1)
+functions.ER_CENTROID.DrawCopy('SAME')
+functions.NR_CENTROID.SetLineColor(ROOT.kBlack)
+functions.NR_CENTROID.SetLineWidth(1)
+functions.NR_CENTROID.DrawCopy('SAME')
 c1.cd(3)
 signal_hist.Draw('CONT0')
 signal_hist.SetStats(0)
 signal_hist.SetContour(30)
-realdata_graph.Draw('SAMESP')
-Functions.ER_centroid.DrawCopy('SAME')
-Functions.NR_centroid.DrawCopy('SAME')
+realdata_scatter.Draw('SAMESP')
+functions.ER_CENTROID.DrawCopy('SAME')
+functions.NR_CENTROID.DrawCopy('SAME')
 c1.cd(2)
 ionframe.Draw()
 c1.cd(4)
