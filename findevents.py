5c73df0d073dd60c92acb578b6b4040da7ca3cd8
LukasHehn <lukas.hehn@ik.fzk.de>
2013-09-16 10:53:44 +0200

    Removed eventset with additional cut to simplify module a bit

--- findevents.py
+++ findevents.py
@@ -16,10 +16,9 @@ from ROOT import TGraph, gROOT, TCanvas, KDataReader
 
 
 # Definition of parameters used for skimming event set
-DETECTOR_NAME = 'ID401'
-#KDataFile = 'Data/Run12_ID3_bckg_with_subrecords.root'
+DETECTOR_NAME = 'ID3'
 KDataFile = 'Data/Run12_ID3+6+401+404_bckg_with_subrecords.root'
-OutFileName = False  # 'Data/ID3_eventlist_lowE_corrected.txt'
+OutFileName = False  # 'Data/ID3_eventlist_ion-rec-only.txt'
 WIMP_MASS = 8  # if wimp mass set, show also signal density of wimp signal
 
 
@@ -43,8 +42,10 @@ FWHM_HEAT = parameters.ENERGY_RESOLUTIONS_ERIC[DETECTOR_NAME]['Heat']
 FWHM_ION = parameters.ENERGY_RESOLUTIONS_ERIC[DETECTOR_NAME]['Fiducial']
 FWHM_REC = functions.fwhm_rec_from_heat(FWHM_HEAT, VOLTAGE, 10)
 GAMMA_CUT = 1.96  # gamma cut from ER centroid in standard deviations
+
 print 'Voltage={voltage}V, threshold={thresh:.2f}keVnr, FWHM_ion={ion}keVee, FWHM_heat={heat}keVee, FWHM_rec={rec:.2f}keVnr\n'.format(
     voltage=VOLTAGE, thresh=E_THRESH_NR, ion=FWHM_ION, heat=FWHM_HEAT, rec=FWHM_REC)
+
 
 # Get dictionary with cuts for all baselines
 CUTS = BASELINE_CUTS_ERIC[DETECTOR_NAME]
@@ -52,8 +53,7 @@ CUTS = BASELINE_CUTS_ERIC[DETECTOR_NAME]
 
 # Define empty lists for event storage as well as reset event counters
 TimeList, RecList, IonList = [], [], []  # for events passing all cuts
-TimeListCuts, RecListCuts, IonListCuts = [], [], []  # for events failing additional cut
-eventcounter, cutcounter = 0, 0
+eventcounter = 0
 
 
 # Here events are selected from a KData file
@@ -97,39 +97,29 @@ for entry in range(entries):
            and CUTS['Guard2']['Min'] < bolo.GetBaselineNoiseGuard(2) < CUTS['Guard2']['Max']\
            and bolo.GetEventFlag() == 2\
            and bolo.GetVoltageFlag >= 0\
-           and bolo.GetIonPulseTimeOffset() < 600:
+           and bolo.GetIonPulseTimeOffset() < 600\
+           and bolo.GetChi2Flag():
 
             EnergyRec = functions.recoil_energy_estimator(EnergyHeat, VOLTAGE)  # estimate recoil energy
 
             if 0 < EnergyIon < E_ION_MAX and 0 < EnergyRec < E_REC_MAX:
+
+                TimeList.append(UnixTime)
+                IonList.append(EnergyIon)
+                RecList.append(EnergyRec)
+                eventcounter += 1
+
+                Comment = ' '
 
                 if eventcounter%20 == 0:
                     print '-'*100
                     print '{0:10} | {1:12} | {2:12} | {3:10} | {4:10} | {5:10} | {6:10}'.format('FileEntry', 'EventNumber', 'UnixTime', 'EnergyHeat', 'EnergyRec', 'EnergyIon', 'Comment')
                     print '-'*100
 
-                if EnergyIon < 2.:
-                    Comment = 'Ion < 2keV'
-                else:
-                    Comment = ' '
-
-                if bolo.GetChi2Flag():
-                    TimeList.append(UnixTime)
-                    IonList.append(EnergyIon)
-                    RecList.append(EnergyRec)
-                    eventcounter += 1
-                else:
-                    Comment = "failed GetChi2Flag"
-                    TimeListCuts.append(UnixTime)
-                    IonListCuts.append(EnergyIon)
-                    RecListCuts.append(EnergyRec)
-                    cutcounter += 1
-
                 print '{0:10} | {1:12} | {2:12} | {3:10.2f} | {4:10.2f} | {5:10.2f} | {6:10}'.format(entry, EventNumber, UnixTime, EnergyHeat, EnergyRec, EnergyIon, Comment)
 infile.Close()
 
 print '{events} events passed all standard cuts'.format(events=eventcounter)
-print '{events} events failed additional cut'.format(events=cutcounter)
 
 
 # Write events in txt-file
@@ -137,11 +127,12 @@ if OutFileName:
     OutFile = open(OutFileName, 'w')
     for i in range(len(TimeList)):
         UnixTime = TimeList[i]
-        YearTime = unixtime_to_year(UnixTime)
+        YearTime = functions.unixtime_to_year(UnixTime)
         EnergyRec = RecList[i]
         EnergyIon = IonList[i]
-        OutFile.write("%s %s %s \n" % (YearTime, EnergyRec, EnergyIon))
-        OutFile.close()
+        #OutFile.write("%s %s %s \n" % (YearTime, EnergyRec, EnergyIon))
+        OutFile.write("%s %s \n" % (EnergyRec, EnergyIon))
+    OutFile.close()
     print 'Outfile %s written to disc' % OutFileName
 
 
@@ -155,18 +146,6 @@ EventGraph.GetXaxis().SetTitle('E_{rec} (keVnr)')
 EventGraph.GetYaxis().SetTitle('E_{ion} (keVee)')
 EventGraph.SetMarkerStyle(ROOT.kFullDotLarge)
 EventGraph.SetMarkerColor(ROOT.kBlack)
-
-
-# Same for additional set of special cut events
-EventGraphCuts = TGraph()
-for i in range(cutcounter):
-    EnergyRec = RecListCuts[i]
-    EnergyIon = IonListCuts[i]
-    EventGraphCuts.SetPoint(i, EnergyRec, EnergyIon)
-EventGraphCuts.GetXaxis().SetTitle('E_{rec} (keVnr)')
-EventGraphCuts.GetYaxis().SetTitle('E_{ion} (keVee)')
-EventGraphCuts.SetMarkerStyle(ROOT.kFullDotLarge)
-EventGraphCuts.SetMarkerColor(ROOT.kMagenta)
 
 
 # Get efficiency and flat gamma background
@@ -201,16 +180,16 @@ if WIMP_MASS:
 
 
 # Plot PDF, data and ER and NR centroids
-c1 = TCanvas('c1', 'Event selection for {detector}'.format(detector=DETECTOR_NAME), 600, 600)
+c1 = TCanvas('c1', 'Event selection for {detector}'.format(detector=DETECTOR_NAME), 800, 600)
 if signal:
     bckgd_plus_sig.Draw('CONT0Z')
 else:
     gamma_bckgd.Draw('CONT0Z')
 EventGraph.Draw('SAMEP')
-#EventGraphCuts.Draw('SAMEP')
+
 
 functions.ER_CENTROID.SetParameter(0, VOLTAGE)
-functions.ER_CENTROID.SetLineColor(ROOT.kBlue)
+functions.ER_CENTROID.SetLineColor(ROOT.kBlack)
 functions.ER_CENTROID.SetLineWidth(3)
 functions.ER_CENTROID.Draw('SAME')
 functions.NR_CENTROID.SetLineColor(ROOT.kBlack)
@@ -218,13 +197,14 @@ functions.NR_CENTROID.SetLineWidth(3)
 functions.NR_CENTROID.SetLineStyle(1)
 functions.NR_CENTROID.Draw('SAME')
 
+
 # Plot Gamma cut line
 if GAMMA_CUT:
     functions.GAMMA_CUT.FixParameter(0, VOLTAGE)
     functions.GAMMA_CUT.FixParameter(2, FWHM_ION/2.35)
     functions.GAMMA_CUT.FixParameter(3, FWHM_REC/2.35)
     functions.GAMMA_CUT.FixParameter(4, GAMMA_CUT)
-    functions.GAMMA_CUT.SetLineColor(ROOT.kBlue)
+    functions.GAMMA_CUT.SetLineColor(ROOT.kBlack)
     functions.GAMMA_CUT.SetLineWidth(3)
     functions.GAMMA_CUT.SetLineStyle(7)
     functions.GAMMA_CUT.Draw('SAME')
